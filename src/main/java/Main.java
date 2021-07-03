5383a03a032798d8ec6bf71b657d93afb8b28a16
threehalves-pi <1.5pi.statshelper@gmail.com>
2021-07-03 12:33:53 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) throws IOException, LoginException {
+        String token = new String(
+                Objects.requireNonNull(Main.class.getResourceAsStream("/token.txt"))
+                        .readAllBytes());
+
+        JDA jda = JDABuilder.createDefault(token)
+                .build();
+    }
+}
