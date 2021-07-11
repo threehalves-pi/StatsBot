73cea927a3f3cf2df386d9cc8a3cdb1deab9962b
threehalves-pi <1.5pi.statshelper@gmail.com>
2021-07-10 20:42:39 -0400

    Changed INFO color to solid white

--- src/main/java/data/Colors.java
+++ src/main/java/data/Colors.java
@@ -1,9 +1,9 @@
 package data;
 
-import java.awt.*;
+import java.awt.Color;
 
 public class Colors {
     public static final Color ADMIN = new Color(0xFEE75C);
     public static final Color ANNOUNCEMENTS = new Color(0x57F287);
-    public static final Color INFO = new Color(0x5865F2);
+    public static final Color INFO = new Color(0xFFFFFF);
 }
